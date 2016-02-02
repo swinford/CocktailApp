983faa74ac92868bbe3d4c85c688299f7afdb713
Zachary Swinford <swinford@seawolf.sonoma.edu>
2016-02-02 14:24:21 -0800

    Added NSManagedObject subclass of CoreData

---
+++ CocktailApp/Cabinet.swift
@@ -0,0 +1,17 @@
+//
+//  Cabinet.swift
+//  CocktailApp
+//
+//  Created by Zachary Swinford on 2/2/16.
+//  Copyright © 2016 Swinford. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Cabinet: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
