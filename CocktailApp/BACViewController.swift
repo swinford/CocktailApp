93cc1c4ceb615013edd5b16745adadf3168c99c6
Brian Crosser <briancrosser@Brians-MacBook-Pro.local>
2016-02-17 20:04:14 -0800

    BAC Updates. Invalid time handling and core data on segue still needed along with testing

--- CocktailApp/BACViewController.swift
+++ CocktailApp/BACViewController.swift
@@ -10,31 +10,32 @@ import UIKit
 import Foundation
 import QuartzCore
 
-class BACViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
-    @IBOutlet weak var invalidWeight: UILabel!
+class BACViewController: UIViewController,UIPickerViewDelegate {
     var startDate: NSDate?
     @IBOutlet weak var gender: UISegmentedControl!
-    @IBOutlet weak var weight: UIPickerView!
-    var weightData = ["50", "60" ,"70", "80", "90", "100", "200"]
+    
+    
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var editWeight: UITextField!
     
     let male: Double = 0.73
     let female: Double = 0.66
     let beerABV: Double = 0.045
     let wineABV: Double = 0.116
     let shotABV: Double = 0.37
+    var weightFinal: Double = 0.0
 
     
     @IBOutlet weak var TextViewTime: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        weight.dataSource = self
-        weight.delegate = self
         shotControllerOutlet.minimumValue = 0
         beerControllerOutlet.minimumValue = 0
         wineControllerOutlet.minimumValue = 0
-        invalidWeight.layer.cornerRadius = 5
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.addTarget(self, action: "didTapView")
+        self.view.addGestureRecognizer(tapRecognizer)
     }
     
     @IBAction func editTimeButton(sender: UIButton) {
@@ -51,14 +52,6 @@ class BACViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDel
         DateFormatter.timeStyle = .ShortStyle
         let temp = DateFormatter.stringFromDate(date)
         return temp
-    }
-
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return weightData[row]
-    }
-    
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        weightLabel.text = weightData[row]
     }
     
     //Shots controller
@@ -83,6 +76,18 @@ class BACViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDel
         wine.text = Int(sender.value).description
     }
     
+    func getWeight() ->Double {
+        //Gets weight
+        weightFinal = 0.0
+        if editWeight.text != "" {
+            weightFinal = Double(Int(self.editWeight.text!)!)
+            return weightFinal
+        }
+        else{
+            return -100.0
+        }
+    }
+    
     @IBOutlet weak var bac: UILabel!
     @IBAction func calculateButton(sender: UIButton) {
         let currentDate = NSDate()
@@ -99,50 +104,51 @@ class BACViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDel
         var minDif = (hourCurrent - hourStart) * 60
         minDif = minDif + minuteCurrent - minuteStart
         
-        //Determines the gender of the user
-        var genderMultiplier: Double
-        if gender.selected == false{
-            genderMultiplier = 0.73
+        let weight: Double = getWeight()
+        if weight != -100.0{
+            //Determines the gender of the user
+            var genderMultiplier: Double
+            if gender.selected == false{
+                genderMultiplier = 0.73
+            }
+            else{
+                genderMultiplier = 0.66
+            }
+        
+            //Gets number or ounces consumed
+            var abv = Double(beers.text!)! * 12 * beerABV
+            abv = abv + Double(shots.text!)! * 1.5 * shotABV
+            abv = abv + Double(wine.text!)! * 5 * wineABV
+        
+            var bacNum: Double = abv * 5.14 / weight * genderMultiplier
+            bacNum = bacNum - 0.015 * Double(minDif)/60.0
+            bacNum = round(1000 * bacNum) / 1000
+            if bacNum < 0{
+                bacNum = 0
+            }
+            bac.text = String(abs(bacNum))
+            
+            if bacNum <= 0.03{
+                bac.textColor = UIColor.greenColor()
+            }
+            else if bacNum <= 0.05 && bacNum > 0.03{
+                bac.textColor = UIColor.yellowColor()
+            }
+            else{
+                bac.textColor = UIColor.redColor()
+            }
+
         }
         else{
-            genderMultiplier = 0.66
-        }
-        
-        //Gets number or ounces consumed
-        var abv = Double(beers.text!)! * 12 * beerABV
-        abv = abv + Double(shots.text!)! * 1.5 * shotABV
-        abv = abv + Double(wine.text!)! * 5 * wineABV
-        
-        //Gets weight
-        var weightFinal: Double = 0.0
-        if weightLabel.text! != "Weight"{
-            weightFinal = Double(weightLabel.text!)!
-        }
-        else{
-            invalidWeight.hidden = false
-            sleep(5)
-            invalidWeight.hidden = true
-        }
-        
-        var bacNum: Double = abv * 5.14 / weightFinal * genderMultiplier
-        bacNum = bacNum - 0.015 * Double(minDif)/60.0
-        bacNum = round(1000 * bacNum) / 1000
-        if bacNum < 0{
-            bacNum = 0
-        }
-        bac.text = String(bacNum)
-        
-        if bacNum <= 0.03{
-            bac.textColor = UIColor.greenColor()
-        }
-        else if bacNum <= 0.05 && bacNum > 0.03{
-            bac.textColor = UIColor.yellowColor()
-        }
-        else{
+            bac.text = "Invalid Weight!"
             bac.textColor = UIColor.redColor()
         }
         
         bac.hidden = false
+    }
+    
+    func didTapView(){
+        self.view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
@@ -151,20 +157,16 @@ class BACViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDel
 }
 
 
-/*
+
 // MARK: - Navigation
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 // Get the new view controller using segue.destinationViewController.
 // Pass the selected object to the new view controller.
+
+
 }
-*/
-    //MARK: - Delegates and data sources
-    //MARK: Data Sources
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return weightData.count
-    }
+
+
+
 }
